d18f877122de39bc237e49e6955179352da19128
Ariel F <ariel.incgarcia@gmail.com>
2023-02-09 18:16:55 -0800

    access control: allow view of items

--- yii/biblioteca/views/libro/lista.php
+++ yii/biblioteca/views/libro/lista.php
@@ -9,13 +9,13 @@ use yii\widgets\LinkPager;
     <?php foreach ($libros as $libro) : ?>
 
         <div class="card" style="width: 18rem;">
-            <?= Html::img($libro->imagen, ['width' => '100px', 'class' => 'card-img-top']); ?>
+            <?= Html::img($libro->imagen, ['class' => 'card-img-top']); ?>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <?= Html::tag('h5', Html::encode($libro -> titulo), ['class'=>'card-title']) ?>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             <div class="card-body">
-                <?= Html::encode($libro->titulo) ?>
+                <?= Html::tag('a',Html::encode($libro->titulo),['class'=>'card-link'])?>
             </div>
         </div>
 
